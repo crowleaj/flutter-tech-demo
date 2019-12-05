83f4f2284d4cc833793c54eecdb387f90716daad
crowleaj <alex.crowley@lifesharetech.com>
2019-12-05 13:54:40 -0500

    Cleanup

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_flutter/alert-icon.dart';
 import 'package:hello_flutter/styled-button.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -18,12 +19,49 @@ class _HomeScreen extends State<HomeScreen> {
     "1:30 PM    Salon Appointment With Marge"
   ];
 
-  String _alertCountText() {
-    return 10.toString();
+  int _alertCount() {
+    return 10;
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget picturesVideoContent = Column(children: [
+      Container(
+          padding: EdgeInsets.all(2),
+          decoration: BoxDecoration(border: Border.all()),
+          child: Image.asset("assets/images/o'hooligan.jpeg")),
+      Align(
+          alignment: Alignment.bottomLeft,
+          child: StyledButton(text: "Pictures & Videos"))
+    ]);
+
+    Widget eventBuilder = ListView.builder(
+        itemCount: events.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+              padding: EdgeInsets.all(8), child: Text(events[index]));
+        });
+
+    Widget eventContent = Column(children: <Widget>[
+      Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+              padding: EdgeInsets.all(5),
+              child: Text("What's Next", style: TextStyle(fontSize: 20)))),
+      Padding(
+        child: Column(children: [
+          SizedBox(height: 100, child: eventBuilder),
+          Align(
+              alignment: Alignment.bottomLeft,
+              child: StyledButton(
+                text: "Calendar",
+                onPressed: () => Navigator.pushNamed(context, "/appointments"),
+              ))
+        ]),
+        padding: EdgeInsets.only(left: 20, right: 20),
+      )
+    ]);
+
     return Scaffold(
       appBar: AppBar(title: Text("Hello, Maggie!")),
       body: ListView(
@@ -32,42 +70,9 @@ class _HomeScreen extends State<HomeScreen> {
         tiles: [
           Card(
               child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(children: [
-              Container(
-                  padding: EdgeInsets.all(2),
-                  decoration: BoxDecoration(border: Border.all()),
-                  child: Image.asset("assets/images/o'hooligan.jpeg")),
-              Align(
-                  alignment: Alignment.bottomLeft,
-                  child: StyledButton(text: "Pictures & Videos"))
-            ]),
-          )),
+                  padding: EdgeInsets.all(10), child: picturesVideoContent)),
           Card(
-            child: Column(children: <Widget>[
-              Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                      padding: EdgeInsets.all(5), child: Text("What's Next", style: TextStyle(fontSize: 20)))),
-              Padding(
-                child: Column(children: [
-                  SizedBox(
-                      height: 100,
-                      child: ListView.builder(
-                        itemCount: events.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Padding(
-                              padding: EdgeInsets.all(8),
-                              child: Text(events[index]));
-                        },
-                      )),
-                  Align(
-                      alignment: Alignment.bottomLeft,
-                      child: StyledButton(text: "Calendar", onPressed: () => Navigator.pushNamed(context, "/appointments"),))
-                ]),
-                padding: EdgeInsets.only(left: 20, right: 20),
-              )
-            ]),
+            child: eventContent,
           ),
           Card(
               child: ListTile(
@@ -84,32 +89,7 @@ class _HomeScreen extends State<HomeScreen> {
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
           BottomNavigationBarItem(
-              icon: Stack(children: [
-                Icon(Icons.notifications),
-                new Positioned(
-                  right: 0,
-                  child: new Container(
-                    padding: EdgeInsets.all(1),
-                    decoration: new BoxDecoration(
-                      color: Colors.purple,
-                      borderRadius: BorderRadius.circular(6),
-                    ),
-                    constraints: BoxConstraints(
-                      minWidth: 12,
-                      minHeight: 12,
-                    ),
-                    child: new Text(
-                      _alertCountText(),
-                      style: new TextStyle(
-                        color: Colors.white,
-                        fontSize: 10,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                )
-              ]),
-              title: Text("Alerts")),
+              icon: AlertIcon(_alertCount()), title: Text("Alerts")),
           BottomNavigationBarItem(
               icon: Icon(Icons.view_headline), title: Text("IDK")),
         ],
