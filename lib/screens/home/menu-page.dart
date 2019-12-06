5d806192107e441640f71f129a7cc07bb02975e8
crowleaj <alex.crowley@lifesharetech.com>
2019-12-06 14:31:44 -0500

    rudimentary YouTube widget

--- lib/screens/home/menu-page.dart
+++ lib/screens/home/menu-page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    YoutubePlayerController _controller = YoutubePlayerController(
+      initialVideoId: 'iLnmTe5Q2Qw',
+      flags: YoutubePlayerFlags(
+        autoPlay: false,
+        mute: true,
+      ),
+    );
+    _controller.setSize(Size.fromHeight(20));
     // TODO: implement build
-    return Text("HERE!");
+    return SizedBox(
+        height: 200,
+        width: 50,
+        child: Card(
+          child: YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              progressColors: ProgressBarColors(
+                playedColor: Colors.amber,
+                handleColor: Colors.amberAccent,
+              )),
+        ));
   }
-
 }
