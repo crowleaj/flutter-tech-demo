a332e3f7ef8c40f134833ca8efd824d638bca7ad
crowleaj <alex.crowley@lifesharetech.com>
2019-12-09 12:42:11 -0500

    Image downloading, YouTube

--- lib/screens/home/menu-page.dart
+++ lib/screens/home/menu-page.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_downloader/image_downloader.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class MenuPage extends StatelessWidget {
+  downloadImage() async {
+    try {
+      // Saved with this method.
+      var dest = AndroidDestinationType.directoryDCIM;
+      dest.subDirectory("Lifeshare");
+      var imageId = await ImageDownloader.downloadImage("https://raw.githubusercontent.com/wiki/ko2ic/image_downloader/images/flutter.png",
+      destination: dest);
+      if (imageId == null) {
+        return;
+      }
+
+      // Below is a method of obtaining saved image information.
+      var fileName = await ImageDownloader.findName(imageId);
+      var path = await ImageDownloader.findPath(imageId);
+      var size = await ImageDownloader.findByteSize(imageId);
+      var mimeType = await ImageDownloader.findMimeType(imageId);
+      print(path + fileName);
+    } on PlatformException catch (error) {
+      print(error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    // downloadImage();
     YoutubePlayerController _controller = YoutubePlayerController(
-      initialVideoId: 'iLnmTe5Q2Qw',
+      initialVideoId: '9BP7Sk4uFu0',
       flags: YoutubePlayerFlags(
         autoPlay: false,
         mute: true,
+        forceHideAnnotation: true,
       ),
     );
     _controller.setSize(Size.fromHeight(20));
     // TODO: implement build
-    return SizedBox(
-        height: 200,
-        width: 50,
-        child: Card(
-          child: YoutubePlayer(
-              controller: _controller,
-              showVideoProgressIndicator: true,
-              progressColors: ProgressBarColors(
-                playedColor: Colors.amber,
-                handleColor: Colors.amberAccent,
-              )),
-        ));
+    return Center(
+        child: SizedBox(
+      height: 150,
+      width: 150,
+      child: YoutubePlayer(
+          controller: _controller,
+          showVideoProgressIndicator: true,
+          progressColors: ProgressBarColors(
+            playedColor: Colors.amber,
+            handleColor: Colors.amberAccent,
+          )),
+    ));
   }
 }
